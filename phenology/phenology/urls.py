92d619215ea4a75ff326291b7c6a4ee05d05d7a4
Julien Marinescu <jma@makina-corpus.com>
2014-07-10 19:24:33 +0200

    register models in admin

--- phenology/phenology/urls.py
+++ phenology/phenology/urls.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 
 from rest_framework import routers
 from backend import views
+from backend.models import Individual, Area, Observer, Organization, Survey, Snowing, Species
 
 admin.autodiscover()
+admin.site.register(Area)
+admin.site.register(Species)
+admin.site.register(Individual)
+admin.site.register(Observer)
+admin.site.register(Organization)
+admin.site.register(Survey)
+admin.site.register(Snowing)
+
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
