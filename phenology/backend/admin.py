0b5470356111196514f7f2385273f5941357933f
Julien Marinescu <jma@makina-corpus.com>
2014-07-15 14:05:54 +0200

    refactor admin

--- phenology/backend/admin.py
+++ phenology/backend/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from backend import models
+
+admin.site.register(models.Area)
+admin.site.register(models.Species)
+admin.site.register(models.Individual)
+admin.site.register(models.Observer)
+admin.site.register(models.Stage)
+admin.site.register(models.Survey)
+admin.site.register(models.Snowing)
+
 
 # Register your models here.
+#class ObserverAdmin(admin.ModelAdmin):
+#    pass
--- phenology/phenology/urls.py
+++ phenology/phenology/urls.py
@@ -6,14 +6,6 @@ from backend import views
 from backend.models import Individual, Area, Observer, Organization, Survey, Snowing, Species
 
 admin.autodiscover()
-admin.site.register(Area)
-admin.site.register(Species)
-admin.site.register(Individual)
-admin.site.register(Observer)
-admin.site.register(Organization)
-admin.site.register(Survey)
-admin.site.register(Snowing)
-
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
