300a1746d4591bc431a3b1f904f16f7514442b89
Julien Marinescu <jma@makina-corpus.com>
2014-07-15 14:08:15 +0200

    refactor models

--- phenology/backend/models.py
+++ phenology/backend/models.py
@@ -19,24 +19,48 @@ class Species(models.Model):
         verbose_name = "espèce"
         verbose_name_plural = "espèces"
 
+    def __str__(self):
+        return self.name
 ###########
+
+
+#zone:
+class Area(models.Model):
+    name = models.CharField(max_length=100, verbose_name="nom")
+    codezone = models.CharField(max_length=20, verbose_name="code zone")
+    polygone = models.CharField(max_length=500, verbose_name="polygone sig")
+    lat = models.FloatField(verbose_name="lattitude")
+    lon = models.FloatField(verbose_name="longitude")
+    remark = models.CharField(max_length=100, verbose_name="remarque", blank=True)
+    commune = models.CharField(max_length=100, verbose_name="commune")
+    species = models.ManyToManyField(Species, blank=True)
+
+    class Meta:
+        verbose_name = "zone observation"
+        verbose_name_plural = "zones observation"
+
+    def __str__(self):
+        return "Area : %s [%s]" % (self.name, self.commune)
 
 
 #observateur
 class Observer(models.Model):
+    user = models.OneToOneField(User)
     city = models.CharField(max_length=100, verbose_name="commune")
     fonction = models.CharField(max_length=70)
     adresse = models.CharField(max_length=80)
     codepostal = models.CharField(max_length=6, verbose_name="code postal")
     nationality = models.CharField(max_length=100, verbose_name="nationalité")
     phone = models.CharField(max_length=20)
-    user = models.OneToOneField(User)
     is_crea = models.BooleanField(verbose_name="est un membre de crea")
+    areas = models.ManyToManyField(Area, verbose_name="Zones", blank=True)
 
     class Meta:
         verbose_name = "observateur"
         verbose_name_plural = "observateurs"
 
+    def __str__(self):
+        return self.user.username
 
 """
 #organisme
@@ -52,30 +76,14 @@ class Organization(models.Model):
         verbose_name_plural = "organismes"
 """
 
-
-#zone:
-class Area(models.Model):
-    name = models.CharField(max_length=100, verbose_name="nom")
-    codezone = models.CharField(max_length=20, verbose_name="code zone")
-    polygone = models.CharField(max_length=500, verbose_name="polygone sig")
-    lat = models.FloatField(verbose_name="lattitude")
-    lon = models.FloatField(verbose_name="longitude")
-    remark = models.CharField(max_length=100, verbose_name="remarque", blank=True)
-    commune = models.CharField(max_length=100, verbose_name="commune")
-    observers = models.ManyToManyField(Observer, verbose_name="Observateurs", blank=True)
-    species = models.ManyToManyField(Species, blank=True)
-
-    class Meta:
-        verbose_name = "zone observation"
-        verbose_name_plural = "zones observation"
-
-
 ##########
+
 
 #individu
 class Individual(models.Model):
     species = models.ForeignKey(Species, verbose_name="espece")
     area = models.ForeignKey(Area, verbose_name="zone")
+    observer = models.ForeignKey(Observer)
     lat = models.FloatField(verbose_name="lattitude")
     lon = models.FloatField(verbose_name="longitude")
     remark = models.CharField(max_length=100, verbose_name="remarque")
@@ -83,6 +91,9 @@ class Individual(models.Model):
     class Meta:
         verbose_name = "individu"
         verbose_name_plural = "individus"
+
+    def __str__(self):
+        return "%s %s [%s]" % (self.species.name, self.area.name, self.observer)
 
 
 #enneigement
@@ -119,3 +130,4 @@ class Survey(models.Model):
     class Meta:
         verbose_name = "observation"
         verbose_name_plural = "observations"
+
