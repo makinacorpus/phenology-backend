86d3843caf16ca5249ef23ae591f4a62ef0f9bbe
Julien Marinescu <jma@makina-corpus.com>
2014-10-21 17:14:38 +0200

    clean up

--- phenology/scripts/resetDb.py
+++ phenology/scripts/resetDb.py
@@ -1,8 +1,9 @@
 from backend.sampledb import create_sample_data
-from django.core.management.sql import sql_delete
 from django.contrib.auth.models import User
 
-def run():
-    User.objects.create_superuser(username = 'admin', email = 'admin@admin.fr', password = 'admin')
-    create_sample_data()
 
+def run():
+    User.objects.create_superuser(username='admin',
+                                  email='admin@admin.fr',
+                                  password='admin')
+    create_sample_data()
