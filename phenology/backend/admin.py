45d601d4120243d71db8d85f0c8ae461a37585f3
Julien Marinescu <jma@makina-corpus.com>
2014-12-18 23:31:01 +0100

    inline observer fieldset

--- phenology/backend/admin.py
+++ phenology/backend/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from backend import models
+from django.contrib.auth.admin import UserAdmin
 
 admin.site.register(models.Area)
 admin.site.register(models.Species)
@@ -8,3 +9,20 @@ admin.site.register(models.Observer)
 admin.site.register(models.Stage)
 admin.site.register(models.Survey)
 admin.site.register(models.Snowing)
+
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class EmployeeInline(admin.StackedInline):
+    model = models.Observer
+    can_delete = False
+    verbose_name_plural = 'observer'
+
+
+# Define a new User admin
+class UserAdmin(UserAdmin):
+    inlines = (EmployeeInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(models.User)
+admin.site.register(models.User, UserAdmin)
