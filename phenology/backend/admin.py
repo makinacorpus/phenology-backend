3ce632b8693ec1835e8938bfe03c189e371e67a6
Julien Marinescu <jma@makina-corpus.com>
2015-01-13 11:42:26 +0100

    improve admin

--- phenology/backend/admin.py
+++ phenology/backend/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from backend import models
 from django.contrib.auth.admin import UserAdmin
+from modeltranslation.admin import TranslationAdmin
 
 admin.site.register(models.Area)
-admin.site.register(models.Species)
+admin.site.register(models.Species, TranslationAdmin)
 admin.site.register(models.Individual)
 admin.site.register(models.Observer)
-admin.site.register(models.Stage)
 admin.site.register(models.Survey)
 admin.site.register(models.Snowing)
+
+
+class StageAdmin(TranslationAdmin):
+    list_display = ('name', 'species', 'order', 'is_active', )
+    ordering = ('species', 'order', 'name')
+
+admin.site.register(models.Stage, StageAdmin)
 
 
 # Define an inline admin descriptor for Employee model
