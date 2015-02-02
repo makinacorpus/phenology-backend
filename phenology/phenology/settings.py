8690f30c731fffaffffb7a1046e74dc07ef7b88d
Julien Marinescu <jma@makina-corpus.com>
2015-02-03 00:15:21 +0100

    disable xframe middleware

--- phenology/phenology/settings.py
+++ phenology/phenology/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE_CLASSES = (
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.gzip.GZipMiddleware',
 )
