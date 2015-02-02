eeb95a4526aad8ca24e4339b231da1246105db34
Julien Marinescu <jma@makina-corpus.com>
2015-02-02 22:52:05 +0100

    add news fields in observer model

--- phenology/backend/models.py
+++ phenology/backend/models.py
@@ -139,6 +139,21 @@ class Area(models.Model):
             geojson["features"].append(ind.geojson())
         return geojson
 
+NATIONALITY_CHOICES = (
+    ('france', _('french')),
+    ('italie', _('italian')),
+)
+
+CATEGORY_CHOICES = (
+    ('particulier', _('particulier')),
+    ('etablissement_scolaire', _('etablissement_scolaire')),
+    ('espace_protege', _('espace_protege')),
+    ('association', _('association')),
+    ('professionnelle', _('professionnelle')),
+    ('centre_decouverte', _('centre_decouverte')),
+    ('autre', _('autre')),
+)
+
 
 #observateur
 class Observer(models.Model):
@@ -146,14 +161,25 @@ class Observer(models.Model):
     city = models.CharField(max_length=100, verbose_name=_("city"))
     fonction = models.CharField(max_length=70, verbose_name=_("fonction"))
     adresse = models.TextField(max_length=80, verbose_name=_("adresse"))
-    codepostal = models.IntegerField(verbose_name=_("codepostal"))
-    nationality = models.CharField(max_length=100, verbose_name=_("nationality"))
+    codepostal = models.CharField(max_length=20, verbose_name=_("codepostal"))
+    nationality = models.CharField(max_length=100,
+                                   verbose_name=_("nationality"),
+                                   choices=NATIONALITY_CHOICES)
+    organism = models.CharField(max_length=150, verbose_name=_("organism"),
+                                blank=True)
+    category = models.CharField(max_length=80, verbose_name=_("category"),
+                                choices=CATEGORY_CHOICES)
     phone = models.CharField(max_length=20, verbose_name=_("phone"))
     mobile = models.CharField(max_length=20, verbose_name=_("mobile"))
-    is_crea = models.BooleanField(verbose_name=_("is a crea member ?"), default=False)
-    is_active = models.BooleanField(verbose_name=_("is activated?"), default=True)
-    areas = select2_fields.ManyToManyField(Area, verbose_name=_("Areas"), blank=True)
-    date_inscription = models.DateField(blank=True, null=True, verbose_name=_("Date joined"))
+    is_crea = models.BooleanField(verbose_name=_("is a crea member ?"),
+                                  default=False)
+    is_active = models.BooleanField(verbose_name=_("is activated?"),
+                                    default=False)
+    areas = select2_fields.ManyToManyField(Area, verbose_name=_("Areas"),
+                                           blank=True)
+    date_inscription = models.DateField(blank=True, null=True,
+                                        verbose_name=_("Date joined"),
+                                        default=datetime.datetime.now)
 
     class Meta:
         verbose_name = _("Observer")
