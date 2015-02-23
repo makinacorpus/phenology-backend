849f7b1430a79f4e4416f0f473d7490897fcb08f
Julien Marinescu <jma@makina-corpus.com>
2015-02-23 21:44:39 +0100

    fix static_root

--- phenology/phenology/settings.py
+++ phenology/phenology/settings.py
@@ -154,7 +154,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
