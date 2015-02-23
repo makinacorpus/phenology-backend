ff452079a28635b4cadcb1fa58a538755b486d19
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-02-23 16:22:54 +0100

    add settings

--- phenology/phenology/settings.py
+++ phenology/phenology/settings.py
@@ -155,6 +155,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 STATIC_URL = '/static/'
 
 TILES_SETTINGS = {
