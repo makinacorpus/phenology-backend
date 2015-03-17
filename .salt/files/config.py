e421d5a47f15eee7ac6664cd043f03d19c9c5553
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-03-17 14:31:31 +0100

    WIP: refactor settings

--- .salt/files/config.py
+++ .salt/files/config.py
@@ -1,111 +1,69 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import (print_function,
+                        division,
+                        absolute_import)
 __docformat__ = 'restructuredtext en'
-{% set cfg = salt['mc_utils.json_load'](cfg) %}
-{% set data = cfg.data %}
-{% macro renderbool(opt)%}
-{{opt}} = {%if data.get(opt, False)%}True{%else%}False{%endif%}
-{% endmacro %}
-import json
-import os
-from django.utils.translation import gettext_lazy as _
-SITE_ID={{data.SITE_ID}}
-SERVER_EMAIL = DEFAULT_FROM_EMAIL = 'root@{{cfg.fqdn}}'
-DATABASES = json.loads("""
-{{salt['mc_utils.json_dump'](data.db)}}
-""".strip())
-{% set admint = None %}
+{%- set cfg = salt['mc_project.get_configuration'](cfg) %}
+{%- set data = cfg.data %}
+
+{%- macro render_setting(setting, value=None) %}
+{%- set setting = setting.strip() %}
+{%- if setting == 'ALLOWED_HOSTS' %}
+{%-    for i in data.server_aliases %}
+{%-      if i not in value %}
+{%-         do value.append(i) %}
+{%-      endif %}
+{%-    endfor %}
+{%- endif %}
+{% if setting == 'ADMINS' %}
 ADMINS = (
-    {% for dadmins in data.admins %}
-    {% for admin, data in dadmins.items() %}
-    {% if data %}{% set admint = (admin, data) %}{%endif %}
-    ('{{admin}}', '{{data.mail}}'),
+    {% for dadmins in data.admins -%}
+    {% for admin, adata in dadmins.items() -%}
+    ('{{admin}}', '{{adata.mail}}'),{# jinja indentation whitespace-#}
     {% endfor %}
     {% endfor %}
 )
-{{renderbool('DEBUG') }}
-{% for i in data.server_aliases %}
-{% if i not in data.ALLOWED_HOSTS %}
-{% do data.ALLOWED_HOSTS.append(i) %}
-{% endif %}
+{% elif setting in [
+    'LANGUAGES',
+    'ADDITIONAL_TEMPLATE_DIRS',
+] %}
+{{setting}} = (
+{%  for v in value -%}
+        {{v-}},
 {% endfor %}
-CORS_ORIGIN_ALLOW_ALL = {{data.CORS_ORIGIN_ALLOW_ALL}}
-ALLOWED_HOSTS = {{data.ALLOWED_HOSTS}}
-DEFAULT_FROM_EMAIL = '{{data.adminmail}}'
-MEDIA_ROOT = '{{data.media}}'
-STATIC_ROOT = '{{data.static}}'
-SECRET_KEY = '{{data.SECRET_KEY}}'
-USE_X_FORWARDED_HOST={{data.USE_X_FORWARDED_HOST}}
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-DATE_FORMAT = '{{data.DATE_FORMAT}}'
-TIME_ZONE = '{{data.timezone}}'
-LANGUAGE_CODE = '{{data.LANGUAGE_CODE}}'
-LANGUAGES = (
-    ('fr', _('Français')),
-    ('it', _('Italia')),
-    ('en', _('English'))
 )
-TILES_SETTINGS = {
-    'TILES_RADIUS_LARGE': 0.01,  # ~1 km
-    'TILES_RADIUS_SMALL': 0.05,  # ~500 m
-    'TILES_GLOBAL_ZOOMS': range(5, 11),  # zoom 7 to 10
-    'TILES_AREA_ZOOMS': range(11, 17),  # zoom 11 to 16
-    'TILES_ROOT': os.path.join(MEDIA_ROOT, 'tiles'),
-    'TILES_URL': 'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png',  # tiles url
-    'GLOBAL_MAP_BBOX': [4.669189453125, 43.69965122967144, 7.9046630859375, 46.464349400461124],  # Whole Alpes
-}
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-        'django.request': {
-            'handlers': ['console', 'mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'django': {
-            'handlers': ['console', 'mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        }
-    }
-}
-{% if data.get('ADDITIONAL_TEMPLATE_DIRS', None) %}
-ADDITIONAL_TEMPLATE_DIRS = tuple({{data.ADDITIONAL_TEMPLATE_DIRS}})
-{% endif %}
+{% elif salt['mc_utils.is_a_set'](value)
+      or salt['mc_utils.is_a_list'](value)
+      or salt['mc_utils.is_a_dict'](value) %}
+try:
+    {{setting}} = json.loads("""{{salt['mc_utils.json_dump'](value, pretty=True)}}""".strip())
+except ValueError:
+    try:
+        {{setting}} = json.loads("""{{salt['mc_utils.json_dump'](value)}}""".strip())
+    except ValueError:
+        {{setting}} = json.loads("""{{salt['mc_utils.json_dump'](value)}}""".replace('\n', '\\n').strip())
+{% elif (
+    salt['mc_utils.is_a_bool'](value)
+    or salt['mc_utils.is_a_number'](value)
+)%}
+{{-setting}} = {{value}}
+{% elif salt['mc_utils.is_a_str'](value) %}
+{{-setting}} = """{{value}}"""
+{% else %}
+{{-setting}} = {{value}}
+{%- endif %}
+{%- endmacro %}
 
-EMAIL_HOST = '{{data.EMAIL_HOST}}'
-EMAIL_HOST_USER = '{{data.EMAIL_HOST_USER}}'
-EMAIL_HOST_PASSWORD = '{{data.EMAIL_HOST_PASSWORD}}'
-EMAIL_PORT = '{{data.EMAIL_PORT}}'
-EMAIL_USE_TLS = '{{data.EMAIL_USE_TLS}}'
-
-# Application specific settings
-{% for param, value in data.get('extra_settings', {}).items() %}
-{{param}} = {{value}}
+import json
+from django.utils.translation import gettext_lazy as _
+{% for setting, value in data.get('django_settings', {}).items() -%}
+{{render_setting(setting, value)}}
+{%- endfor %}
+{% for setting, value in data.get('extra_django_settings', {}).items() -%}
+{{render_setting(setting, value)}}
+{%- endfor %}
+{% for setting, value in data.get('extra_settings', {}).items() %}
+{{setting}} = {{value}}
 {% endfor %}
 # vim:set et sts=4 ts=4 tw=80:
-
-
-
