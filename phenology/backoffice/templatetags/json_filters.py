a8f4cec7a1e7f08c48a3dc5357ad91fe1f1f7c29
Julien Marinescu <jma@makina-corpus.com>
2015-03-26 02:41:45 +0100

    improve json_filters

--- phenology/backoffice/templatetags/json_filters.py
+++ phenology/backoffice/templatetags/json_filters.py
@@ -3,10 +3,12 @@ from django.core.serializers import serialize
 from django.db.models.query import QuerySet
 from django.utils import simplejson
 from backoffice import register
+from backoffice.utils import json_serial
+
 
 def jsonify(object):
     if isinstance(object, QuerySet):
         return serialize('json', object)
-    return simplejson.dumps(object)
+    return simplejson.dumps(object, default=json_serial)
 
 register.filter('jsonify', jsonify)
