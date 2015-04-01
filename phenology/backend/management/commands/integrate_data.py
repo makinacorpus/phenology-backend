f11cbb601395351e71e973f19b6254b64239d7d7
Julien Marinescu <jma@makina-corpus.com>
2015-04-02 01:34:17 +0200

    changes on script

--- phenology/backend/management/commands/integrate_data.py
+++ phenology/backend/management/commands/integrate_data.py
@@ -3,9 +3,28 @@
 
 import json
 from django.core.management.base import BaseCommand
+from django.db.models import Q
 
 from backend import logger
 from backend import models
+
+mapping = {'meleze': 'Mélèze',
+           'epicea': 'Epicéa',
+           'bouleauv': 'Bouleau verruqueux',
+           'bouleaup': 'Bouleau pubescent',
+           'frene': 'Frêne',
+           'noisetier': 'Noisetier',
+           'tussilage': 'Tussilage',
+           'primevere': 'Primevère',
+           'sorbier': 'Sorbier',
+           'lilas': 'Lilas',
+           'debourrement': 'Débourrement',
+           'floraison': 'Floraison',
+           'feuillaison': 'Feuillaison',
+           'couleur_deb': 'Changement couleur début',
+           'couleur_moit': 'Changement couleur moitié',
+           'chute_fin': 'Chute des feuilles fin',
+           'chute_moit': 'Chute des feuilles moitié'}
 
 
 class Command(BaseCommand):
@@ -13,7 +32,7 @@ class Command(BaseCommand):
 
     def execute(self, *args, **options):
         """ Execute command """
-        filename = "backup_db.json"
+        filename = "backup_db4.json"
         self.integrate_dump_in_db(filename)
         logger.info('Done.')
 
@@ -21,6 +40,7 @@ class Command(BaseCommand):
         """ integrate data in new db """
         outfile = open(filename)
         main_results = json.load(outfile)
+        """
         for species in main_results["species"]:
             species["name"] = species["name"].capitalize()
             if not models.Species.objects.filter(name=species["name"]):
@@ -32,10 +52,8 @@ class Command(BaseCommand):
                     stage_tmp = models.Stage()
                     stage_tmp.name = stages["stade"]
                     stage_tmp.order = i
-                    stage_tmp.date_start = stages.get("date_start")
                     stage_tmp.month_start = stages.get("month_start")
                     stage_tmp.day_start = stages.get("day_start")
-                    stage_tmp.date_end = stages.get("date_end")
                     stage_tmp.month_end = stages.get("month_end")
                     stage_tmp.day_end = stages.get("day_end")
                     stage_tmp.species = species_tmp
@@ -44,7 +62,7 @@ class Command(BaseCommand):
                 species_tmp = models.Species.objects\
                     .filter(name=species["name"]).first()
             species_tmp.save()
-
+        """
         for code in sorted(main_results["codes"].keys()):
             """if code != "ronce":
                 continue"""
@@ -67,10 +85,12 @@ class Command(BaseCommand):
                     observer = models.Observer()
                     observer.user = user_tmp
                     observer.city = user["ville_obs"]
-                    observer.codepostal = user["codepostal_obs"]
+                    observer.codepostal = user["codepostal_obs"].strip()
+                    if observer.codepostal.isdigit() and len(observer.codepostal) == 4:
+                        observer.codepostal = "0%s" % observer.codepostal
                     observer.nationality = user["nationalite_obs"]
                     observer.phone = user["tel_obs"]
-                    observer.function = user["fonction_obs"]
+                    observer.fonction = user["fonction_obs"]
                     observer.organism = user["organisme"]
                     observer.category = user["categorie"]
                     observer.adresse = user["adresse_obs"]
@@ -85,7 +105,6 @@ class Command(BaseCommand):
             areas = main_results["codes"][code]["areas"]
 
             for area in areas:
-                print area["name"]
                 if not models.Area.objects.filter(name=area["name"]):
                     area_tmp = models.Area()
                     area_tmp.name = area["name"]
@@ -93,7 +112,9 @@ class Command(BaseCommand):
                     area_tmp.lon = str(area.get("lon")).replace(",",".")
                     area_tmp.altitude = area.get("altitude")
                     area_tmp.commune = area.get("commune", "")
-                    area_tmp.postalcode = area.get("code_postal", "")
+                    area_tmp.postalcode = area.get("code_postal", "").strip()
+                    if area_tmp.postalcode.isdigit() and len(area_tmp.postalcode) == 4:
+                        area_tmp.postalcode = "0%s" % area_tmp.postalcode
                 else:
                     area_tmp = models.Area.objects.filter(name=area["name"]).first()
                 area_tmp.save()
@@ -107,15 +128,6 @@ class Command(BaseCommand):
                         snowing_tmp.remark = temp["remark_temp"]
                         snowing_tmp.height = temp["neige_haut"]
                         snowing_tmp.save()
-                    if(temp["temperature"] < 99):
-                        snowing_tmp = models.Temperature()
-                        snowing_tmp.area = area_tmp
-                        snowing_tmp.observer = observers[0]
-                        snowing_tmp.date = "%s %s" % (temp["date_releve"],
-                                                      temp["heure_releve"])
-                        snowing_tmp.remark = temp["remark_temp"]
-                        snowing_tmp.temperature = temp["temperature"]
-                        snowing_tmp.save()
                 for observer in observers:
                     if(observer not in area_tmp.observer_set.all()):
                         area_tmp.observer_set.add(observer)
@@ -125,11 +137,11 @@ class Command(BaseCommand):
                 for area_species in area["species"]:
                     species_m = [sp for sp in main_results["species"]
                                  if sp["name"] == area_species["name"].
-                                 capitalize()]
+                                 capitalize() or sp["name"] == area_species["name"]]
                     species = models.Species.objects\
-                        .filter(name=species_m[0]["name"]).first()
-                    if not(species in area_tmp.species.all()):
-                        area_tmp.species.add(species)
+                        .filter(Q(name=species_m[0]["name"]) | Q(name=mapping[species_m[0]["name"]])).first()
+                    if not species:
+                        print area_species["name"]
                     for ind in area_species["individuals"]:
                         if(not models.Individual.objects.filter(name=ind["name"]).filter(area_id=area_tmp.id).filter(species_id=species.id)):
                             ind_tmp = models.Individual()
@@ -140,6 +152,10 @@ class Command(BaseCommand):
                             ind_tmp.altitude = ind.get("altitude")
                             ind_tmp.lat = ind.get("lat", "1")
                             ind_tmp.lon = ind.get("lon", "-1")
+                            ind_tmp.milieu = ind.get("milieu")
+                            ind_tmp.pente = ind.get("pente")
+                            ind_tmp.exposition = ind.get("exposition")
+                            ind_tmp.remark = ind.get('remark_zone')
                             ind_tmp.save()
                         else:
                             ind_tmp = models.Individual.objects.filter(name=ind["name"]).filter(area_id=area_tmp.id).filter(species_id=species.id).first()
@@ -157,7 +173,7 @@ class Command(BaseCommand):
                             survey_tmp.answer = "isObserved"
                             survey_tmp.date = survey["date"]
                             try:
-                                survey_tmp.stage = models.Stage.objects.filter(name=survey["stade"]).filter(species_id=species.id).first()
+                                survey_tmp.stage = models.Stage.objects.filter(Q(name=survey["stade"]) | Q(name=mapping[survey["stade"]])).filter(species_id=species.id).first()
                             except Exception as e:
                                 print e
                             survey_tmp.save()
